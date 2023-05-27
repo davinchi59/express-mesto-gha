f94587a75fae5424c8babb8069890a522855fb7f
davinchi59 <mirnuy1296@mail.ru>
2023-05-27 11:35:01 +0700

    ПР 14.1

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,5 +1,10 @@
+const celebrate = require('celebrate');
+const IncorrectDataError = require('../errors/IncorrectDataError');
+
 module.exports = (err, req, res, next) => {
-  if (err.statusCode) {
+  if (celebrate.isCelebrateError(err)) {
+    res.status(400).send({ message: 'Переданы неверные данные' });
+  } else if (err.statusCode) {
     console.log(err.statusCode, err.message);
     res.status(err.statusCode).send({ message: err.message });
   } else {
