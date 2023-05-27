7932a7eb762e7986d6a5eaf73d7fc18f224d034e
davinchi59 <mirnuy1296@mail.ru>
2023-05-27 11:24:20 +0700

    ПР 14.1

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,7 +1,9 @@
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   if (err.statusCode) {
+    console.log(err.statusCode, err.message);
     res.status(err.statusCode).send({ message: err.message });
   } else {
     res.status(500).send({ message: err.message });
   }
+  next();
 };
